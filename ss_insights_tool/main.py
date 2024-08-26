4ebe60e87f0496a7ea1eac509c4d8428bead7e58
YKH2020 <yash.hegde.2020@gmail.com>
2024-08-26 10:06:14 -0400

    added functions for showing graphs for gender and age, then clearing them.

---
+++ ss_insights_tool/main.py
@@ -0,0 +1,15 @@
+'''
+Questions:
+TODO 1: Frequency of Anxiety Levels Across Universities, Departments, and Academic Years? Did receiving a scholarship/waiver mitigate the anxiety?
+TODO 2: How does the level of anxiety and GPA correlate with the level of stress among students? Does higher anxiety often lead to higher stress levels?
+TODO 3: What leads to depression? Higher anxiety, higher stress, or both? (use survey questions)
+
+COMPLETED e: What are the ages of everyone here?
+COMPLETED e: What is the gender demographic?
+'''
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
